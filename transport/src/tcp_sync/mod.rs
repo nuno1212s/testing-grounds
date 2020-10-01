a1ac646e5861194e43feaf2f915f1d77c718d6e2
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-01 15:52:57 +0100

    wip: working on write bench for tcp conns

--- transport/src/tcp_sync/mod.rs
+++ transport/src/tcp_sync/mod.rs
@@ -1,9 +1,23 @@
 use bencher::Bencher;
 
+use std::thread;
+use std::sync::mpsc;
+
 pub fn bench_write(b: &mut Bencher) {
-    let mut x = 0;
+    let (tx, rx) = sync_channel(0);
+    thread::spawn(move || {
+        const ADDR: &str = "127.0.0.1:12345";
+        let listener = TcpListener::bind(ADDR)
+            .expect("failed to listen on " + ADDR);
+        for stream in listener.incoming() {
+            // make this identical for each test
+            handle_client(stream?);
+        }
+        tx.send(()).unwrap();
+    });
     b.iter(|| {
         // placeholder
         x += 1;
     });
+    rx.recv().unwrap()
 }
