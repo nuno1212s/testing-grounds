39df3b61c2475673d3cbb54d46741a83d02328fc
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-05 18:30:05 +0100

    old uncommitted changes

--- transport/src/handlers/mod.rs
+++ transport/src/handlers/mod.rs
@@ -1,13 +1,24 @@
+use std::sync::atomic::AtomicI32;
 use std::io::{self, Read, Write};
+use std::time::{Instant, Duration};
+
+const BUFSIZ: usize = 4096;
 
 pub fn handle_read_sync<R: Read>(mut r: R) -> io::Result<()> {
-    let mut buf = [0_u8; 8];
+    let mut buf = [0_u8; BUFSIZ];
     r.read(&mut buf[..])?;
     Ok(())
 }
 
 pub fn handle_write_sync<W: Write>(mut w: W) -> io::Result<()> {
-    let mut buf = [0_u8; 8];
+    let mut buf = [0_u8; BUFSIZ];
     w.write(&mut buf[..])?;
     Ok(())
+}
+
+fn run_for_one_second_sync<F>(f: F) -> i32
+where
+    F: FnMut(),
+{
+    asd
 }
