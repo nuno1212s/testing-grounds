363b6ca16c5f785168c740d8e40dfa26b261163a
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-12 22:00:27 +0100

    change package name

--- bft-smart/src/main/java/bftsmartest/App.java
+++ bft-smart/src/main/java/bftsmartest/App.java
@@ -1,19 +1,30 @@
-package bftsmart;
+package bftsmartest;
 
-import bftsmartest.map.*;
+import bftsmartest.map.MapServer;
+import bftsmartest.map.MapInteractiveClient;
 
 public class App {
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.err.printf("Usage: %s <client|server> <id>\n", args[0]);
-            System.exit(1);
+        if (args.length < 2) {
+            usage();
         }
 
-        final int nodeId = Integer.parseInt(args[2]);
+        final int nodeId = Integer.parseInt(args[1]);
 
-        switch (args[1]) {
+        switch (args[0]) {
         case "client":
+            MapInteractiveClient.main(nodeId);
             break;
+        case "server":
+            MapServer.main(nodeId);
+            break;
+        default:
+            usage();
         }
+    }
+
+    public static void usage() {
+        System.err.println("Usage: App <client|server> <id>");
+        System.exit(1);
     }
 }
