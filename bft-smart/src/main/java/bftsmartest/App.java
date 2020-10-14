655b4007e42199897368a9b7ac60dfc373a72a7d
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-14 18:44:28 +0100

    log to file

--- bft-smart/src/main/java/bftsmartest/App.java
+++ bft-smart/src/main/java/bftsmartest/App.java
@@ -1,7 +1,13 @@
 package bftsmartest;
 
+import java.io.IOException;
+
 import bftsmartest.map.MapServer;
 import bftsmartest.map.MapInteractiveClient;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.SimpleLayout;
+import org.apache.log4j.BasicConfigurator;
 
 public class App {
     public static void main(String[] args) {
@@ -10,6 +16,13 @@ public class App {
         }
 
         final int nodeId = Integer.parseInt(args[1]);
+
+        try {
+            BasicConfigurator.configure(new FileAppender(new SimpleLayout(), String.format("log/%d", nodeId), false));
+        } catch (IOException e) {
+            System.err.println("Failed to create log file.");
+            System.exit(1);
+        }
 
         switch (args[0]) {
         case "client":
