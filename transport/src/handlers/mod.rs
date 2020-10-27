2b8d9894074b713f1e295e2a1e1ae1c670bdc951
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-27 14:49:49 +0000

    async handlers

--- transport/src/handlers/mod.rs
+++ transport/src/handlers/mod.rs
@@ -1,5 +1,6 @@
 use super::params;
 use std::io::{self, Read, Write};
+use futures::io::{AsyncRead, AsyncWrite, AsyncReadExt, AsyncWriteExt};
 
 pub fn handle_read_sync<R: Read>(mut r: R) -> io::Result<()> {
     let mut buf = [0_u8; params::BUFSIZ];
@@ -10,5 +11,17 @@ pub fn handle_read_sync<R: Read>(mut r: R) -> io::Result<()> {
 pub fn handle_write_sync<W: Write>(mut w: W) -> io::Result<()> {
     let mut buf = [0_u8; params::BUFSIZ];
     w.write(&mut buf[..])?;
+    Ok(())
+}
+
+pub async fn handle_read_async<R: AsyncRead + Unpin>(mut r: R) -> io::Result<()> {
+    let mut buf = [0_u8; params::BUFSIZ];
+    r.read(&mut buf[..]).await?;
+    Ok(())
+}
+
+pub async fn handle_write_async<W: AsyncWrite + Unpin>(mut w: W) -> io::Result<()> {
+    let mut buf = [0_u8; params::BUFSIZ];
+    w.write(&mut buf[..]).await?;
     Ok(())
 }
