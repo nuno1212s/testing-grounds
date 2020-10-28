ce1a1a148c310d7d8cea7cf5e7a0a3149c8bc9be
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-28 21:05:08 +0000

    simplify this

--- transport/src/nodes/mod.rs
+++ transport/src/nodes/mod.rs
@@ -1,19 +1,13 @@
 use std::io::{self, Read, Write};
 //use futures::io::{AsyncRead, AsyncWrite}; TODO: async traits
 
-pub trait Client: Read + Write
-where
-    Self: Sized,
-{
+pub trait Client: Read + Write + Sized {
     type Addr;
 
     fn connect_server(addr: Self::Addr) -> io::Result<Self>;
 }
 
-pub trait Server
-where
-    Self: Sized,
-{
+pub trait Server: Sized {
     type Client: Client;
 
     fn listen_clients(addr: <<Self as Server>::Client as Client>::Addr) -> io::Result<Self>;
