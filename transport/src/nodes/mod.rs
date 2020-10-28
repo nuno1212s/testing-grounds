0a3b108c99e1809172e9d73cb1a2b00784c61c3c
Tiago Carvalho <up201605259@fc.up.pt>
2020-10-28 21:08:40 +0000

    async traits

--- transport/src/nodes/mod.rs
+++ transport/src/nodes/mod.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Read, Write};
-//use futures::io::{AsyncRead, AsyncWrite}; TODO: async traits
+
+use async_trait::async_trait;
+use futures::io::{AsyncRead, AsyncWrite};
 
 pub trait Client: Read + Write + Sized {
     type Addr;
@@ -12,4 +14,19 @@ pub trait Server: Sized {
 
     fn listen_clients(addr: <<Self as Server>::Client as Client>::Addr) -> io::Result<Self>;
     fn accept_client(&self) -> io::Result<Self::Client>;
+}
+
+#[async_trait]
+pub trait AsyncClient: AsyncRead + AsyncWrite + Sized {
+    type Addr;
+
+    async fn connect_server_async(addr: Self::Addr) -> io::Result<Self>;
+}
+
+#[async_trait]
+pub trait AsyncServer: Sized {
+    type Client: AsyncClient;
+
+    async fn listen_clients_async(addr: <<Self as AsyncServer>::Client as AsyncClient>::Addr) -> io::Result<Self>;
+    async fn accept_client_async(&self) -> io::Result<Self::Client>;
 }
