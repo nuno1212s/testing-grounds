7bfd79f2fa3a724a815c30841c9376ede34c29a7
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-23 16:43:58 +0000

    change ip

--- layered/src/main.rs
+++ layered/src/main.rs
@@ -2,8 +2,11 @@ const SRC: &str = "0.0.0.0:5555";
 const DST: &str = "127.0.0.1:5555";
 
 fn main() {
+    let dst = std::env::var("IP")
+        .map(|ip| format!("{}:5555", ip))
+        .unwrap_or_else(|_| DST.into());
     match std::env::args().nth(1).as_ref().map(String::as_ref) {
-        Some("client") => ditto::bench::layered_bench(ditto::bench::Side::Client, DST),
+        Some("client") => ditto::bench::layered_bench(ditto::bench::Side::Client, &dst),
         Some("server") => ditto::bench::layered_bench(ditto::bench::Side::Server, SRC),
         _ => panic!("Arg must be either client or server"),
     }
