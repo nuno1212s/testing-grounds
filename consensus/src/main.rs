63d03d5839601c06595ac2bea3a896e4c6203843
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-08 15:49:11 +0000

    progress

--- consensus/src/main.rs
+++ consensus/src/main.rs
@@ -21,14 +21,20 @@ struct Config {
 
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 enum ErrorKind {
-    Disconnected(u32),
+    DisconnectedTx(u32),
+    DisconnectedRx(u32),
+}
+
+enum Message {
+    System(SystemMessage),
+    ConnectedTx(u32, TcpStream),
+    ConnectedRx(u32, TcpStream),
+    Error(ErrorKind),
 }
 
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
-enum Message {
+enum SystemMessage {
     Consensus(ConsensusMessage),
-    Connected(u32, TcpStream),
-    Error(ErrorKind),
 }
 
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
@@ -74,9 +80,8 @@ struct Node {
     id: u32,
     addrs: Vec<SocketAddr>,
     others_tx: HashMap<u32, Arc<TcpStream>>,
-    others_rx: HashMap<u32, Arc<TcpStream>>,
-    my_tx: Arc<mpsc::Sender<ConsensusMessage>>,
-    my_rx: Arc<Mutex<mpsc::Receiver<ConsensusMessage>>>,
+    my_tx: mpsc::Sender<Message>,
+    my_rx: mpsc::Receiver<Message>,
 }
 
 #[derive(Debug)]
@@ -133,20 +138,17 @@ impl System {
 
         let listener = TcpListener::bind(cfg.addrs[id as usize]).await?;
         let mut others_tx = HashMap::new();
-        let mut others_rx = HashMap::new();
 
-        let (tx, mut rx) = mpsc::channel(8);
+        let (tx, mut rx) = mpsc::channel(32);
 
         // rx side (accept conns from replica)
         let tx_clone = tx.clone();
         tokio::spawn(async move {
-            // TODO: spawn handler for conn, get messages on a single channel shared
-            // across all tasks
             let tx = tx_clone;
             loop {
                 if let Ok((mut conn, _)) = listener.accept().await {
                     let id = conn.read_u32().await.unwrap();
-                    tx.send(CommSide::Rx((id, conn))).await.unwrap_or(());
+                    tx.send(Message::ConnectedRx(id, conn)).await.unwrap_or(());
                 }
             }
         });
@@ -160,7 +162,7 @@ impl System {
                 for _ in 0..4 {
                     if let Ok(mut conn) = TcpStream::connect(addr).await {
                         conn.write_u32(id).await.unwrap();
-                        tx.send(CommSide::Tx((other_id, conn))).await.unwrap();
+                        tx.send(Message::ConnectedTx(other_id, conn)).await.unwrap();
                         return;
                     }
                     tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
@@ -170,14 +172,20 @@ impl System {
         }
 
         for _ in 0..(2 * (n - 1)) {
-            let (id, conn) = rx.recv()
+            let message = rx.recv()
                 .await
                 .ok_or_else(||
                     io::Error::new(io::ErrorKind::Other, "connection problems!"))?;
-            match received {
-                CommSide::Tx((id, conn)) => others_tx.insert(id, Arc::new(conn)),
-                CommSide::Rx((id, conn)) => others_rx.insert(id, Arc::new(conn)),
-            };
+            match message {
+                Message::ConnectedTx(id, conn) => {
+                    others_tx.insert(id, Arc::new(conn));
+                },
+                Message::ConnectedRx(id, conn) => {
+                    // TODO: spawn handler for conn, get messages on a single channel shared
+                    // across all tasks
+                },
+                _ => unreachable!(),
+            }
         }
 
         let phase = ProtoPhase::Boot;
@@ -221,7 +229,7 @@ impl System {
     async fn propose_value(&self, value: i32) -> io::Result<()> {
         match self.phase {
             ProtoPhase::PrePreparing if self.node.id == self.leader => {
-                let message = self.new_msg(ConsensusMessageKind::PrePrepare(value));
+                let message = self.new_consensus_msg(ConsensusMessageKind::PrePrepare(value));
                 self.node.broadcast(message, 0_u32..self.n);
                 Ok(())
             },
@@ -251,7 +259,7 @@ impl System {
                     },
                 };
                 if self.node.id != self.leader {
-                    let message = self.new_msg(ConsensusMessageKind::Prepare);
+                    let message = self.new_consensus_msg(ConsensusMessageKind::Prepare);
                     self.node.broadcast(message, 0_u32..self.n);
                 }
                 Ok(ProtoPhase::Preparing(0))
@@ -273,7 +281,7 @@ impl System {
                     },
                 };
                 if i == self.quorum() {
-                    let message = self.new_msg(ConsensusMessageKind::Commit);
+                    let message = self.new_consensus_msg(ConsensusMessageKind::Commit);
                     self.node.broadcast(message, 0_u32..self.n);
                     Ok(ProtoPhase::Commiting(0))
                 } else {
@@ -309,8 +317,8 @@ impl System {
         }
     }
 
-    fn new_msg(&self, kind: ConsensusMessageKind) -> ConsensusMessage {
-        ConsensusMessage::new(self.node.id, self.seq, kind)
+    fn new_consensus_msg(&self, kind: ConsensusMessageKind) -> SystemMessage {
+        SystemMessage::Consensus(ConsensusMessage::new(self.node.id, self.seq, kind))
     }
 }
 
