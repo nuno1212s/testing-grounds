2ba6366ae40ad8b3404043fbc661f836436ac2b6
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-11 00:42:06 +0000

    add a few workers

--- tokrio/src/main.rs
+++ tokrio/src/main.rs
@@ -1,15 +1,40 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 use rio::{Rio, Ordering};
+use futures_lite::future;
 use socket2::{Protocol, Socket, Domain, Type};
 use std::net::{TcpStream, TcpListener, SocketAddr};
+use std::fmt::Write;
 use std::io;
+
+const WORKERS: usize = 16;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let ring = rio::new()?;
     let addr = "127.0.0.1:4321".parse().unwrap();
-    let sock = connect(&ring, addr, Ordering::Link).await?;
-    write(&ring, &sock, b"ganda cena mano\n", Ordering::Link).await?;
-    Ok(())
+    for i in 0..WORKERS {
+        let sock = connect(&ring, addr, Ordering::Link).await?;
+        tokio::spawn(write_loop(i, ring.clone(), sock));
+    }
+    future::pending().await
+}
+
+async fn write_loop(id: usize, ring: Rio, sock: TcpStream) {
+    let mut buf = String::new();
+    loop {
+        match write!(&mut buf, "{} -> I'm here!\n", id) {
+            Ok(_) => (),
+            _ => unreachable!(),
+        };
+        let result = write(&ring, &sock, &buf, Ordering::Link)
+            .await;
+        if let Err(_) = result {
+            return;
+        }
+        buf.clear();
+    }
 }
 
 async fn connect(ring: &Rio, addr: SocketAddr, order: Ordering) -> io::Result<TcpStream> {
