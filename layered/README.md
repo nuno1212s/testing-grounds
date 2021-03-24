916cab4bbbb94fb9186ff14fdecf77ec321fe3ce
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-24 21:29:43 +0000

    rename: ditto -> febft

--- layered/README.md
+++ layered/README.md
@@ -1,3 +1,5 @@
 # IMPORTANT
 
-Last compatible commit with ditto was `6038a8ed46bbd14193ffbb3aaadedf764957cdf2`.
+~~Last compatible commit with febft was `6038a8ed46bbd14193ffbb3aaadedf764957cdf2`.~~
+
+Now stated on the `Cargo.toml`.
--- layered/src/main.rs
+++ layered/src/main.rs
@@ -10,8 +10,8 @@ fn main() {
         .map(|ip| format!("{}:5555", ip))
         .unwrap_or_else(|_| DST.into());
     match std::env::args().nth(1).as_ref().map(String::as_ref) {
-        Some("client") => ditto::bench::layered_bench(ditto::bench::Side::Client, &dst),
-        Some("server") => ditto::bench::layered_bench(ditto::bench::Side::Server, SRC),
+        Some("client") => febft::bench::layered_bench(febft::bench::Side::Client, &dst),
+        Some("server") => febft::bench::layered_bench(febft::bench::Side::Server, SRC),
         _ => panic!("Arg must be either client or server"),
     }
 }
