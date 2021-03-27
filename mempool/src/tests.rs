5dfef589933357ef6b25a0971755563086d64c1e
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-27 19:25:35 +0000

    smallvec

--- mempool/src/tests.rs
+++ mempool/src/tests.rs
@@ -1,8 +1,10 @@
 use lazy_static::lazy_static;
+use smallvec::SmallVec;
 use object_pool::Pool;
 
 const POOL_CAP: usize = 2048;
-const BUF_CAP: usize = 8192;
+const BUF_CAP: usize = 16384;
+const TEST_SIZE: usize = 100;
 
 #[cfg_attr(feature = "jemalloc", global_allocator)]
 #[cfg(feature = "jemalloc")]
@@ -26,9 +28,12 @@ fn bench_pool(b: &mut test::Bencher) {
         h.execute(move || {
             let mut work = Work::new(x);
             let mut vec = MEM_POOL.pull(allocate);
-            work.fill(&mut vec);
+            work.fill(&mut vec[..TEST_SIZE]);
         });
         x += 1;
+        if x % 4 == 0 {
+            h.join();
+        }
     });
     thread_pool.join();
 }
@@ -43,15 +48,42 @@ fn bench_std(b: &mut test::Bencher) {
         h.execute(move || {
             let mut work = Work::new(x);
             let mut vec = allocate();
-            work.fill(&mut vec);
+            work.fill(&mut vec[..TEST_SIZE]);
         });
         x += 1;
+        if x % 4 == 0 {
+            h.join();
+        }
+    });
+    thread_pool.join();
+}
+
+#[bench]
+fn bench_smallvec(b: &mut test::Bencher) {
+    let thread_pool = threadpool::Builder::new()
+        .build();
+    let h = thread_pool.clone();
+    let mut x = 0;
+    b.iter(move || {
+        h.execute(move || {
+            let mut work = Work::new(x);
+            let mut vec = allocate_smallvec();
+            work.fill(&mut vec[..TEST_SIZE]);
+        });
+        x += 1;
+        if x % 4 == 0 {
+            h.join();
+        }
     });
     thread_pool.join();
 }
 
 fn allocate() -> Vec<u8> {
     Vec::with_capacity(BUF_CAP)
+}
+
+fn allocate_smallvec() -> SmallVec<[u8; BUF_CAP]> {
+    SmallVec::new()
 }
 
 impl Work {
