4605cbee26ab54fa23217c799993019247a3f147
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-23 18:27:02 +0100

    atone vecdeque

--- ordered-map/src/tests.rs
+++ ordered-map/src/tests.rs
@@ -2,6 +2,7 @@ use std::default::Default;
 use std::marker::PhantomData;
 use std::collections::VecDeque;
 
+use atone::Vc;
 use indexmap::IndexMap;
 use linked_hash_map::LinkedHashMap;
 use twox_hash::RandomXxh3HashBuilder64;
@@ -70,6 +71,44 @@ fn bench_vecdequemap(b: &mut test::Bencher) {
 #[bench]
 fn bench_vecdequemap_unique(b: &mut test::Bencher) {
     let mut m = VecDequeMap::new();
+    let mut k = Key::<32>::new();
+    b.iter(move || {
+        let mut i = 0;
+        let k = loop {
+            let k = k.next().unwrap();
+            unsafe { m.insert_unique(k.clone(), ()); }
+            if i == INS {
+                break k;
+            }
+            i += 1;
+        };
+        m.pop_front();
+        m.remove(&k);
+    });
+}
+
+#[bench]
+fn bench_vcmap(b: &mut test::Bencher) {
+    let mut m = VcMap::new();
+    let mut k = Key::<32>::new();
+    b.iter(move || {
+        let mut i = 0;
+        let k = loop {
+            let k = k.next().unwrap();
+            m.insert(k.clone(), ());
+            if i == INS {
+                break k;
+            }
+            i += 1;
+        };
+        m.pop_front();
+        m.remove(&k);
+    });
+}
+
+#[bench]
+fn bench_vcmap_unique(b: &mut test::Bencher) {
+    let mut m = VcMap::new();
     let mut k = Key::<32>::new();
     b.iter(move || {
         let mut i = 0;
@@ -157,6 +196,69 @@ impl<K, V> VecDequeMap<K, V> {
             Some(v) => v,
             None => unreachable!(),
         };
+        Some(v)
+    }
+
+    fn locate(&mut self, k: &K) -> Option<usize>
+    where
+        K: Eq,
+    {
+        for (i, this_k) in self.keys.iter().enumerate() {
+            if this_k == k {
+                return Some(i);
+            }
+        }
+        None
+    }
+}
+
+struct VcMap<K, V> {
+    keys: Vc<K>,
+    values: Vc<V>,
+}
+
+impl<K, V> VcMap<K, V> {
+    fn new() -> Self {
+        Self {
+            keys: Vc::new(),
+            values: Vc::new(),
+        }
+    }
+
+    fn insert(&mut self, k: K, mut v: V) -> Option<V>
+    where
+        K: Eq,
+    {
+        if let Some(i) = self.locate(&k) {
+            std::mem::swap(&mut self.values[i], &mut v);
+            return Some(v);
+        }
+        // key not present
+        unsafe { self.insert_unique(k, v); }
+        None
+    }
+
+    unsafe fn insert_unique(&mut self, k: K, v: V) {
+        self.keys.push_back(k);
+        self.values.push_back(v);
+    }
+
+    fn pop_front(&mut self) -> Option<(K, V)> {
+        let k = self.keys.pop_front()?;
+        let v = match self.values.pop_front() {
+            Some(v) => v,
+            None => unreachable!(),
+        };
+        Some((k, v))
+    }
+
+    fn remove(&mut self, k: &K) -> Option<V>
+    where
+        K: Eq,
+    {
+        let index = self.locate(k)?;
+        self.keys.remove(index);
+        let v = self.values.remove(index);
         Some(v)
     }
 
