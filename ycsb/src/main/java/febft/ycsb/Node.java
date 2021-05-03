77dcced49d5136be7f70a4a8fb67e9f1ca4fdf9a
Tiago Carvalho <up201605259@fc.up.pt>
2021-05-04 00:51:03 +0100

    wip

--- ycsb/src/main/java/febft/ycsb/Node.java
+++ ycsb/src/main/java/febft/ycsb/Node.java
@@ -1,3 +1,5 @@
+package febft.ycsb;
+
 public class Node {
     public final static int ID = 1000;
 }
--- ycsb/src/main/java/febft/ycsb/YCSBClient.java
+++ ycsb/src/main/java/febft/ycsb/YCSBClient.java
@@ -13,16 +13,17 @@ import org.apache.log4j.SimpleLayout;
 import org.apache.log4j.BasicConfigurator;
 
 import site.ycsb.ByteIterator;
+import site.ycsb.Status;
 import site.ycsb.DB;
 
 public class YCSBClient extends DB {
     private Node node;
 
-    public void init() {
-        if (args.length < 2) {
-            usage();
-        }
+    public YCSBClient() {
+        // empty constructor
+    }
 
+    public void init() {
         try {
             String sep = System.getProperty("file.separator");
             BasicConfigurator.configure(new FileAppender(new SimpleLayout(), String.format("log%s%d", sep, Node.ID), false));
@@ -35,13 +36,29 @@ public class YCSBClient extends DB {
     }
 
     @Override
-    public int delete(String arg0, String arg1) {
-        throw new UnsupportedOperationException();
+    public Status update(String table, String key, Map<String, ByteIterator> values) {
+        // TODO: implement update
+        return Status.NOT_IMPLEMENTED;
     }
 
     @Override
-    public int scan(String arg0, String arg1, int arg2, Set<String> arg3,
-                    Vector<Map<String, ByteIterator>> arg4) {
-        throw new UnsupportedOperationException();
+    public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+        return Status.NOT_IMPLEMENTED;
+    }
+
+    @Override
+    public Status scan(String table, String startkey, int recordcount, Set<String> fields,
+                       Vector<HashMap<String, ByteIterator>> result) {
+        return Status.NOT_IMPLEMENTED;
+    }
+
+    @Override
+    public Status insert(String table, String key, Map<String, ByteIterator> values) {
+        return Status.NOT_IMPLEMENTED;
+    }
+
+    @Override
+    public Status delete(String table, String key) {
+        return Status.NOT_IMPLEMENTED;
     }
 }
