cb2142ddfb79e15edd797e0a3ad63e1717c974da
Tiago Carvalho <up201605259@fc.up.pt>
2021-05-05 18:07:29 +0100

    node id

--- ycsb/src/main/java/febft/ycsb/Node.java
+++ ycsb/src/main/java/febft/ycsb/Node.java
@@ -3,6 +3,12 @@ package febft.ycsb;
 // TLS in Java:
 // https://blog.gypsyengineer.com/en/security/an-example-of-tls-13-client-and-server-on-java.html
 
+impot febft.ycsb.IdCounter;
+
 public class Node {
     private int id;
+
+    public Node() {
+        this.id = IdCounter.nextId();
+    }
 }
