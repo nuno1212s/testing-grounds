43a7fe985d102da5e7bbbc361ac562d8a09fe443
Tiago Carvalho <up201605259@fc.up.pt>
2021-05-05 17:53:51 +0100

    disable netty

--- ycsb/src/main/java/febft/ycsb/Node.java
+++ ycsb/src/main/java/febft/ycsb/Node.java
@@ -1,5 +1,8 @@
 package febft.ycsb;
 
+// TLS in Java:
+// https://blog.gypsyengineer.com/en/security/an-example-of-tls-13-client-and-server-on-java.html
+
 public class Node {
     public final static int ID = 1000;
 }
