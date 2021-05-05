5cf0ae276be4c0c2dc89d4492371d337c6d97dc7
Tiago Carvalho <up201605259@fc.up.pt>
2021-05-05 18:03:33 +0100

    counter

---
+++ ycsb/src/main/java/febft/ycsb/IdCounter.java
@@ -0,0 +1,13 @@
+package febft.ycsb;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class IdCounter {
+    private static AtomicInteger counter = new AtomicInteger();
+
+    public static int nextId() {
+        Properties props = System.getProperties();
+        int initId = Integer.valueOf((String)props.get("smart-initkey"));
+        return initId + counter.addAndGet(1);
+    }
+}
