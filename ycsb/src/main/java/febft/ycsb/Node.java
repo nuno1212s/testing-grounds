88196b553218ba3227a81531816f54652cdb38e9
Tiago Carvalho <up201605259@fc.up.pt>
2021-05-06 19:44:39 +0100

    wip: bootstrap

--- ycsb/src/main/java/febft/ycsb/Node.java
+++ ycsb/src/main/java/febft/ycsb/Node.java
@@ -3,7 +3,11 @@ package febft.ycsb;
 // TLS in Java:
 // https://blog.gypsyengineer.com/en/security/an-example-of-tls-13-client-and-server-on-java.html
 
+import java.util.Map;
+import java.util.HashMap;
 import java.nio.ByteBuffer;
+
+import javax.net.ssl.SSLSocket;
 
 import static febft.ycsb.Config.Entry;
 import febft.ycsb.Config;
@@ -11,12 +15,20 @@ import febft.ycsb.IdCounter;
 
 public class Node {
     private Entry config;
+    private Map<Integer, SSLSocket> tx;
+    private Map<Integer, SSLSocket> rx;
 
     public Node() {
         config = Config.getClients().get(new Integer(IdCounter.nextId()));
     }
 
     public void bootstrap() throws Exception {
+        Map<Integer, Entry> replicas = Config.getReplicas();
+
+        // connect to replicas
+
+
+        // accept conns from replicas
     }
 
     public Entry getConfig() {
