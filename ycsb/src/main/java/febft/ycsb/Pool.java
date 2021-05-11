1b958637e629b84471f751d51813973e4f2cfffd
Tiago Carvalho <up201605259@fc.up.pt>
2021-05-11 16:55:21 +0100

    concurrent pool

--- ycsb/src/main/java/febft/ycsb/Pool.java
+++ ycsb/src/main/java/febft/ycsb/Pool.java
@@ -1,5 +1,7 @@
 package febft.ycsb;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.Future;
 import java.util.concurrent.Callable;
@@ -8,18 +10,36 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ExecutionException;
 
 public class Pool {
+    private static final int F = 1;
+    private static final int QUORUM = 2*F + 1;
+
     private static ExecutorService INSTANCE = Executors.newCachedThreadPool();
+    //private static ExecutorService INSTANCE = Executors.newWorkStealingPool();
 
     // TODO: wait only for a quorum
     public static <T> T call(Collection<? extends Callable<T>> callables) {
         T result = null;
         try {
-            for (Future<T> fut : INSTANCE.invokeAll(callables)) {
-                result = fut.get();
+            List<Future<T>> futures = new ArrayList<>();
+            for (Callable<T> callable : callables) {
+                futures.add(INSTANCE.submit(callable));
             }
-        } catch (ExecutionException | InterruptedException e) {
-            System.err.println("FATAL: Future was interrupted");
+            int resolved = 0;
+            while (resolved < QUORUM) {
+                for (int i = 0; i < futures.size(); i++) {
+                    Future<T> fut = futures.get(i);
+                    if (fut.isDone()) {
+                        result = fut.get();
+                        futures.remove(i);
+                    }
+                }
+                Thread.sleep(100);
+            }
+        } catch (ExecutionException e) {
+            System.err.printf("FATAL: Execution exception: %s", e);
             System.exit(1);
+        } catch (InterruptedException e) {
+            return null;
         } finally {
             return result;
         }
