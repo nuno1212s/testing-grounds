5bedef5d1646a757f1d5bd7699870c3989a96d74
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-06-15 16:20:03 +0100

    wip: client

--- ycsb-smart/src/main/java/bftsmartest/service/Client.java
+++ ycsb-smart/src/main/java/bftsmartest/service/Client.java
@@ -1,14 +1,16 @@
 package bftsmartest.service;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import site.ycsb.RandomByteIterator;
+import site.ycsb.ByteArrayByteIterator;
 import site.ycsb.ByteIterator;
 import site.ycsb.Status;
 
 import bftsmart.tom.ServiceProxy;
 
 public class Client {
+    private static final int FIELD_COUNT = 20000;
     private static final int FIELD_LENGTH = 1024;
     private static final int UPDATE_MAX = 128;
 
@@ -23,10 +25,30 @@ public class Client {
     }
 
     public static void main(String[] args) {
+        AtomicInteger throughput = new AtomicInteger(0);
+        Thread testCase = new Thread(() -> {
+            Client client = new Client(1001);
+            for (;;) {
+                client.update(/* ... */);
+                throughput.getAndAdd(1);
+            }
+        });
+        testCase.start();
+        Thread.sleep(30);
+        System.out.println("Throughput per sec: " + (throughput.get() / 30));
     }
 
     public Status update(String table, String key, Map<String, ByteIterator> values) {
-        byte[] request = null;
-        byte[] reply = serviceProxy.invokeOrdered(request);
+        updates[updateCount++] = new Update(table, key, values);
+
+        if (updateCount % UPDATE_MAX == 0) {
+            updateCount = 0;
+            byte[] request = (new RequestMessage(updates)).serialize().array();
+            byte[] reply = serviceProxy.invokeOrdered(request);
+            RequestMessage message = SystemMessage.deserializeAs(ReplyMessage.class, ByteBuffer.wrap(reply));
+            return message.getStatus();
+        }
+
+        return Status.OK;
     }
 }
