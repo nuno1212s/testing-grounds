4208cdcbe2cd44072d4c86096f38880649b99c69
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-11-04 19:53:57 +0000

    wip: measurements

--- microbenchmarks-rust/src/exec.rs
+++ microbenchmarks-rust/src/exec.rs
@@ -1,13 +1,25 @@
 use std::sync::Arc;
+use std::time::SystemTime;
+use std::default::Default;
 
 use febft::bft::error::*;
-use febft::bft::executable::Service;
+use febft::bft::benchmarks::{
+    BatchMeta,
+    Measurements,
+    BenchmarkHelperStore,
+};
+use febft::bft::executable::{
+    Service,
+    UpdateBatch,
+    UpdateBatchReplies,
+};
 
 use crate::serialize::MicrobenchmarkData;
 
 pub struct Microbenchmark {
-    interval: usize,
+    iterations: usize,
     reply: Arc<Vec<u8>>,
+    measurements: Measurements,
 }
 
 impl Microbenchmark {
@@ -20,7 +32,8 @@ impl Microbenchmark {
 
         Self {
             reply,
-            interval: 0,
+            iterations: 0,
+            measurements: Measurements::default(),
         }
     }
 }
@@ -29,6 +42,9 @@ impl Service for Microbenchmark {
     type Data = MicrobenchmarkData;
 
     fn initial_state(&mut self) -> Result<Vec<u8>> {
+        // TODO: remove this
+        drop(MicrobenchmarkData::REQUEST_SIZE);
+
         Ok((0..)
             .into_iter()
             .take(MicrobenchmarkData::STATE_SIZE)
@@ -36,7 +52,42 @@ impl Service for Microbenchmark {
             .collect())
     }
 
-    fn update(&mut self, _state: &mut Vec<u8>, _request: Vec<u8>) -> Arc<Vec<u8>> {
-        Arc::clone(&self.reply)
+    fn update(&mut self, _s: &mut Vec<u8>, _r: Vec<u8>) -> Arc<Vec<u8>> {
+        unimplemented!()
+    }
+
+    fn update_batch(
+        &mut self,
+        _state: &mut Vec<u8>,
+        batch: UpdateBatch<Vec<u8>>,
+        mut meta: BatchMeta,
+    ) -> UpdateBatchReplies<Arc<Vec<u8>>> {
+        let mut reply_batch = UpdateBatchReplies::with_capacity(batch.len());
+
+        meta.execution_time = SystemTime::now();
+
+        for update in batch.into_inner() {
+            let (peer_id, dig, _req) = update.into_inner();
+            let reply = Arc::clone(&self.reply);
+            reply_batch.add(peer_id, dig, reply);
+        }
+
+        // increase iter count
+        self.iterations += 1;
+
+        // take measurements
+        meta.batch_size.store(&mut self.measurements.batch_size);
+        (meta.consensus_decision_time, meta.consensus_start_time).store(&mut self.measurements.consensus_latency);
+        (meta.consensus_start_time, meta.reception_time).store(&mut self.measurements.pre_cons_latency);
+        (meta.execution_time, meta.consensus_decision_time).store(&mut self.measurements.pos_cons_latency);
+        (meta.prepare_sent_time, meta.consensus_start_time).store(&mut self.measurements.pre_prepare_latency);
+        (meta.commit_sent_time, meta.prepare_sent_time).store(&mut self.measurements.prepare_latency);
+        (meta.consensus_decision_time, meta.commit_sent_time).store(&mut self.measurements.commit_latency);
+
+        if self.iterations % MicrobenchmarkData::MEASUREMENT_INTERVAL == 0 {
+            //
+        }
+
+        reply_batch
     }
 }
