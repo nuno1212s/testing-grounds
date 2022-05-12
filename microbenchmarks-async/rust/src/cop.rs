7fe520bd8756e26b0f74aa37447951e0c22c1711
Nuno Neto <nuno.neto.g@gmail.com>
2022-05-12 19:08:43 +0100

    Attempt to see how long it takes to receive the client requests.

--- microbenchmarks-async/rust/src/cop.rs
+++ microbenchmarks-async/rust/src/cop.rs
@@ -248,7 +248,6 @@ fn sk_stream() -> impl Iterator<Item=KeyPair> {
 }
 
 async fn run_client(client: Client<MicrobenchmarkData>, q: Arc<AsyncSender<String>>) {
-
     let concurrent_rqs: usize = MicrobenchmarkData::CONCURRENT_RQS;
 
     let mut sessions = Vec::with_capacity(concurrent_rqs);
@@ -261,7 +260,7 @@ async fn run_client(client: Client<MicrobenchmarkData>, q: Arc<AsyncSender<Strin
         let mut ramp_up: i32 = 1000;
 
         let mut client = client.clone();
-        
+
         let request = Arc::new({
             let mut r = vec![0; MicrobenchmarkData::REQUEST_SIZE];
             OsRng.fill_bytes(&mut r);
@@ -269,13 +268,13 @@ async fn run_client(client: Client<MicrobenchmarkData>, q: Arc<AsyncSender<Strin
         });
 
         let q = q.clone();
-        
+
         let join_handle = rt::spawn(async move {
             let iterator = 0..(MicrobenchmarkData::OPS_NUMBER / 2 / concurrent_rqs);
 
             for req in iterator {
                 if MicrobenchmarkData::VERBOSE {
-                    print!("Sending req {}...", req);
+                    print!("Sending req {:?}/{}...", client.session(), req);
                 }
 
                 let last_send_instant = Utc::now();
@@ -335,18 +334,17 @@ async fn run_client(client: Client<MicrobenchmarkData>, q: Arc<AsyncSender<Strin
             OsRng.fill_bytes(&mut r);
             r
         });
-        
+
         let q = q.clone();
-        
+
         let mut st = BenchmarkHelper::new(client.id(), MicrobenchmarkData::OPS_NUMBER / 2);
 
         let join_handle = rt::spawn(async move {
-
             let iterator = 0..(MicrobenchmarkData::OPS_NUMBER / 2 / concurrent_rqs);
 
             for req in iterator {
                 if MicrobenchmarkData::VERBOSE {
-                    print!("Sending req {}...", req);
+                    print!("Sending req {:?}/{}...", client.session(), req);
                 }
 
                 let last_send_instant = Utc::now();
@@ -385,7 +383,7 @@ async fn run_client(client: Client<MicrobenchmarkData>, q: Arc<AsyncSender<Strin
                     Delay::new(MicrobenchmarkData::REQUEST_SLEEP_MILLIS).await;
                 }
             }
-            
+
             st
         });
 
@@ -393,7 +391,7 @@ async fn run_client(client: Client<MicrobenchmarkData>, q: Arc<AsyncSender<Strin
     }
 
     let mut st = sessions.pop().unwrap().await.unwrap();
-    
+
     for x in sessions {
         x.await;
     }
