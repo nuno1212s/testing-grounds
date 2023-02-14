905a7f4e161b9f9e6ec0d737d0b2e0f381cf00b5
Nuno Neto <nuno.neto.g@gmail.com>
2023-02-14 16:18:21 +0000

    Small thing.

--- microbenchmarks/rust/build.rs
+++ microbenchmarks/rust/build.rs
@@ -3,6 +3,7 @@ const MESSAGE_CAPNP_SRC: &str = "src/schemas/messages.capnp";
 fn main() {
     // recompile capnp message into rust when the source changes
     println!("cargo:rerun-if-changed={}", MESSAGE_CAPNP_SRC);
+
     capnpc::CompilerCommand::new()
         .src_prefix("src/schemas")
         .file(MESSAGE_CAPNP_SRC)
