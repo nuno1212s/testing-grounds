52a57fdbd3fa47851d384b215a71503e4283cfe0
Nuno Neto <nuno.neto.g@gmail.com>
2023-02-14 18:58:01 +0000

    Update ulimit.

--- microbenchmarks/execute_and_log.sh
+++ microbenchmarks/execute_and_log.sh
@@ -8,6 +8,6 @@ if [[ $# -ge 3 ]]; then
     TO_RUN="$3"
 fi
 
-ulimit -n 100000
+ulimit -n 1000000
 
 rm -rf "${RESULT_FOLDER}" && mkdir -p "${RESULT_FOLDER}" && cp env "${RESULT_FOLDER}/env" && ./run "$TO_RUN" | tee "${RESULT_FOLDER}/log.txt"
