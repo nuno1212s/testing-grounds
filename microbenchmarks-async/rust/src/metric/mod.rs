85b1562a269983acdeb0154297d95265ca33401f
nuno1212s <nuno.neto.g@gmail.com>
2023-06-22 14:49:49 +0100

    Added some metrics to state and log transfer.

--- microbenchmarks-async/rust/src/metric/mod.rs
+++ microbenchmarks-async/rust/src/metric/mod.rs
@@ -1,5 +1,4 @@
-/// This is going to take the 6XX metric ID space
-
+/// This is going to take the 7XX metric ID space
 pub const TIME_TAKEN_TO_DO_TEST: &str = "TIME_TAKEN_TO_DO_TEST";
-pub const TIME_TAKEN_TO_DO_TEST_ID: usize = 600;
+pub const TIME_TAKEN_TO_DO_TEST_ID: usize = 700;
 
