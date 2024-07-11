fbd8e291fc2f0238dae575c60a4a270125934d18
Nuno Neto <nuno.neto.g@gmail.com>
2024-07-11 22:04:34 +0100

    Start working on setting up ansible deployments.

---
+++ microbenchmarks-async/ansible-microbenchmarks-deployments/ansible-compose-generation/src/allocators/round_robin.rs
@@ -0,0 +1,119 @@
+use crate::{AllocatedState, MachineRestrictions, NodeAllocator};
+use std::collections::BTreeMap;
+
+#[derive(Default)]
+pub(crate) struct RoundRobinAllocator;
+
+impl NodeAllocator for RoundRobinAllocator {
+    fn get_node_to_allocate(
+        &self,
+        is_client: bool,
+        current_state: &mut AllocatedState,
+        restrictions: Option<&BTreeMap<String, MachineRestrictions>>,
+    ) -> String {
+        if is_client {
+            allocate_client(current_state, restrictions)
+        } else {
+            allocate_replica(current_state, restrictions)
+        }
+    }
+}
+
+fn allocate_client(
+    current_state: &mut AllocatedState,
+    restrictions: Option<&BTreeMap<String, MachineRestrictions>>,
+) -> String {
+    let nodes = current_state.cluster_state();
+
+    let mut iterations = 0;
+
+    loop {
+        let next_node_round = if let Some(last_allocated_node) = current_state.last_allocated_node() {
+            nodes
+                .range(last_allocated_node.clone()..)
+                .next()
+                .map(|(node, _)| node.clone())
+                .unwrap_or_else(|| {
+                    nodes
+                        .first_key_value()
+                        .expect("Cannot allocate on empty cluster")
+                        .0
+                        .clone()
+                })
+        } else {
+            nodes
+                .first_key_value()
+                .expect("Cannot allocate on empty cluster")
+                .0
+                .clone()
+        };
+
+        if restrictions.is_some_and(|restrictions| meets_restrictions(true, &next_node_round, restrictions, current_state)) {
+            return next_node_round;
+        }
+
+        iterations += 1;
+
+        if iterations >= nodes.len() {
+            panic!("Cannot allocate on any node");
+        }
+    }
+}
+
+fn allocate_replica(
+    current_state: &mut AllocatedState,
+    restrictions: Option<&BTreeMap<String, MachineRestrictions>>,
+) -> String {
+    let nodes = current_state.cluster_state();
+
+    let mut iterations = 0;
+
+    loop {
+        let next_node_round = if let Some(last_allocated_node) = current_state.last_allocated_node() {
+            nodes
+                .range(last_allocated_node.clone()..)
+                .next()
+                .map(|(node, _)| node.clone())
+                .unwrap_or_else(|| {
+                    nodes
+                        .first_key_value()
+                        .expect("Cannot allocate on empty cluster")
+                        .0
+                        .clone()
+                })
+        } else {
+            nodes
+                .first_key_value()
+                .expect("Cannot allocate on empty cluster")
+                .0
+                .clone()
+        };
+
+        if restrictions.is_some_and(|restrictions| meets_restrictions(true, &next_node_round, restrictions, current_state)) {
+            return next_node_round;
+        }
+
+
+        iterations += 1;
+
+        if iterations >= nodes.len() {
+            panic!("Cannot allocate on any node");
+        }
+    }
+}
+
+fn meets_restrictions(
+    client: bool,
+    node: &str,
+    restrictions: &BTreeMap<String, MachineRestrictions>,
+    current_state: &AllocatedState,
+) -> bool {
+    let node_state = current_state.cluster_state().get(node).unwrap();
+    let restrictions = restrictions.get(node).unwrap();
+
+    return if client {
+        node_state.allocated_clients() < restrictions.max_clients()
+    } else {
+        node_state.allocated_replicas() < restrictions.max_replicas()
+    };
+}
